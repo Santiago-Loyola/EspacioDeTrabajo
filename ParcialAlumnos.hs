da8cfe1edad6e543821d49585b1ecd5cd35fc76a
Santiago-Loyola <santieduloyola@gmail.com>
2018-05-31 17:52:36 -0300

    Update ParcialAlumnos.hs

--- ParcialAlumnos.hs
+++ ParcialAlumnos.hs
@@ -4,21 +4,43 @@ import Text.Show.Functions
 type Nombre = String
 type Horas = Float
 type MateriaAprobada = Alumno -> Alumno
+type EfectoAlumno = Alumno -> Alumno
 type Conceptos = String
 type CantReentrega = Float
 
 data Alumno = Alumno {nombre::Nombre,dedicacion::Horas,materiasAprobadas::[MateriaAprobada],conceptos::[Conceptos]}deriving Show
+data Materia = Materia {asignatura::Nombre, efecto::EfectoAlumno } deriving Show
+
 santi = Alumno "Santiago" 0 [] []
 
-agregarConcepto concepto (Alumno nombre horas materias conceptos) = Alumno nombre horas materias (concepto:conceptos)
+paradigmas = Materia {
+asignatura = "paradigmas",
+efecto = (aumentarDedicacion 100 . aprenderConcepto "orden superior" . aprenderConcepto "polimorfismo") }
+
+sistemasOperativos reentregas = Materia {
+asignatura = "sistemas Operativos",
+efecto = agregarPrefijo "Excelentísim@ ".aumentarDedicacion (1000*reentregas)}
+
+recursividadAFull = Materia {
+asignatura = "Recursividad a Full",
+efecto = nuevoConcepto recursividad
+}
+
+
+nuevoConcepto sigConcepto alumno = alumno {conceptos = sigConcepto}
+aprenderConcepto unConcepto alumno = nuevoConcepto (unConcepto: conceptos alumno) alumno
+
 aumentarDedicacion horitas (Alumno nombre horas materias conceptos) = Alumno nombre (horitas+horas) materias conceptos
 agregarMateriaAprobada materia (Alumno nombre horas materias conceptos) = Alumno nombre horas (materia:materias) conceptos
 agregarPrefijo prefijo (Alumno nombre horas materias conceptos) = Alumno (prefijo++nombre) horas materias conceptos
 
 aprobar :: MateriaAprobada -> Alumno -> Alumno
 aprobar materia = agregarMateriaAprobada materia . materia
-paradigmas :: MateriaAprobada
-paradigmas = aumentarDedicacion 100 . agregarConcepto "orden superior" . agregarConcepto "polimorfismo"
+paradigma :: MateriaAprobada
+paradigma = aumentarDedicacion 100 . aprenderConcepto "orden superior" . aprenderConcepto "polimorfismo"
 
-sistemasOperativos :: CantReentrega -> Alumno -> Alumno
-sistemasOperativos reentregas = agregarPrefijo "Excelentísim@ ".aumentarDedicacion (1000*reentregas)
+sistemasOperativo :: CantReentrega -> Alumno -> Alumno
+sistemasOperativo reentregas = agregarPrefijo "Excelentísim@ ".aumentarDedicacion (1000*reentregas)
+
+
+recursividad = map  ((++) "Recursividad " .show) [1..]
