39757fbacbe60d34902bd03e80a2be4b3ae53285
Santiago-Loyola <santieduloyola@gmail.com>
2018-08-14 13:24:20 -0300

    Update Parcial de Lógico Viernes Noche - 18-8-2017.pl

--- Parcial de Lógico Viernes Noche - 18-8-2017.pl
+++ Parcial de Lógico Viernes Noche - 18-8-2017.pl
@@ -1,30 +1,23 @@
 %MAL PLANTEADO LA BASE DE CONOCIMIENTOS
-cantante(megurineluka,nightFever).
-cantante(megurineluka,foreverYoung).
-cantante(hatsuneMiku,tellYourWorld).
-cantante(gumi,foreverYoung).
-cantante(gumi,tellYourWorld).
-cantante(seeU,novemberRain).
-cantante(seeU,nightFever).
-tema(nightFever,4).
-tema(foreverYoung,5).
-tema(tellYourWorld,4).
-tema(foreverYoung,4).
-tema(tellYourWorld,5).
-tema(novemberRain,6).
-tema(nightFever,5).
-
+cantante(megurineluka,nightFever,4).
+cantante(megurineluka,foreverYoung,5).
+cantante(hatsuneMiku,tellYourWorld,4).
+cantante(gumi,foreverYoung,4).
+cantante(gumi,tellYourWorld,5).
+cantante(seeU,novemberRain,6).
+cantante(seeU,nightFever,5).
 
 %Definir los siguientes predicados que sean totalmente inversibles, a menos que se indique lo contrario.
 
 %1)
 esNovedoso(Cantante):-sabeAlMenos2Canciones(Cantante),tiempoTotal(Cantante).
-sabeAlMenos2Canciones(Cantante):-cantante(Cantante,_),findall(Tema,cantante(Cantante,Tema),Canciones),length(Canciones,Cantidad),Cantidad>=2.
-tiempoTotal(Cantante):-cantante(Cantante,Tema),findall(Tiempo,tema(Tema,Tiempo),ListaDeTiempo),sumlist(ListaDeTiempo,Numero),Numero < 15.
+
+sabeAlMenos2Canciones(Cantante):-cantante(Cantante,_,_),findall(Tema,cantante(Cantante,Tema,_),Canciones),length(Canciones,Cantidad),Cantidad>=2.
+tiempoTotal(Cantante):-cantante(Cantante,Tema,_),findall(Tiempo,cantante(Cantante,Tema,Tiempo),ListaDeTiempo),sumlist(ListaDeTiempo,Numero),Numero < 15.
 
 /*
 Hay algunos vocaloids que simplemente no quieren cantar canciones largas porque
 no les gusta, es por eso que se pide saber si un cantante es acelerado, condición
 que se da cuando todas sus canciones duran 4 minutos o menos. Resolver sin usar forall/2.
 */
-esAcelerado(Cantante):-cantante(Cantante,Tema),tema(Tema,Tiempo),findall(Tiempo,(cantante(Cantante,Tema),tema(Tema,Tiempo)),Tiempos),max_member(Numero,Tiempos),Numero =< 4
+esAcelerado(Cantante):-cantante()findall(Tiempo,cantante(Cantante,_,Tiempo),Tiempos),max_member(Numero,Tiempos),4 >= Numero.
